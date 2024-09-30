2a0015fbd2a1d05113b856c75440b8f4c01e8b68
TayfurSafakGencay <safak.gencay@gmail.com>
2024-09-30 13:45:02 +0300

    Particle Manager Created. Hit Zombie effect added.

---
+++ Assets/Scripts/Enemy/BodyPart.cs
@@ -0,0 +1,38 @@
+﻿using Base.Interface;
+using Managers;
+using UnityEngine;
+
+namespace Enemy
+{
+  public class BodyPart : MonoBehaviour, IDamageable
+  {
+    public BodyPartKey BodyPartKey;
+
+    public Rigidbody Rigidbody { get; private set; }
+    
+    private Enemy _enemy;
+
+    private void Awake()
+    {
+      Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void SetManager(Enemy enemy)
+    {
+      _enemy = enemy;
+    }
+
+    public void TakeDamage(int damage, Vector3 forceDirection, Vector3 hitPoint)
+    {
+      ParticleManager.Instance.PlayParticleEffectFromPool(hitPoint, VFX.HitZombie);
+      
+      bool isDead = _enemy.Health.TakeDamage(damage);
+      print(BodyPartKey);
+
+      if (isDead)
+      {
+        _enemy.Ragdoll.Death(damage, forceDirection, hitPoint, Rigidbody);
+      }
+    }
+  }
+}
