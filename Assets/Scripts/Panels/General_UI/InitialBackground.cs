4fce90868ed3f5dca1b9683c513dd03f0934457a
TayfurSafakGencay <safak.gencay@gmail.com>
2024-10-15 20:33:17 +0300

    First Eye open scene added with Volume Profile.

--- Assets/Scripts/Panels/General_UI/InitialBackground.cs
+++ Assets/Scripts/Panels/General_UI/InitialBackground.cs
@@ -1,28 +1,34 @@
 ﻿using DG.Tweening;
 using Managers.Manager.Settings;
 using UnityEngine;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Panels.General_UI
 {
-  [RequireComponent(typeof(Image))]
   public class InitialBackground : MonoBehaviour
   {
-    private Image _backgroundImage;
-
+    [SerializeField]
+    private VolumeProfile _volumeProfile;
+    
+    private Vignette _vignette;
+    
     private void Awake()
     {
-      _backgroundImage = GetComponent<Image>();
+      if(_volumeProfile.TryGet<Vignette>(out Vignette vignette))
+      {
+        _vignette = vignette;
+      }
+      
+      _vignette.intensity.value = 1;
     }
 
     private void Start()
     {
-      _backgroundImage.DOColor(new Color(0, 0, 0, 0.5f), 1.5f).OnComplete(() =>
-        {
-          _backgroundImage.DOColor(new Color(0, 0, 0, 0f), 0.5f).OnComplete(AnimationCompleted);
-        });
-
       SettingsManager.Instance.DisableDeviceControls();
+
+        DOTween.To(() => _vignette.intensity.value, x => _vignette.intensity.Override(x), 
+          0, 2f).OnComplete(AnimationCompleted);
     }
 
     private void AnimationCompleted()
